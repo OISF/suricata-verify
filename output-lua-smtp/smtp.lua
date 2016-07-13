854787cc3c62d830eed46eac6985dc0f4b455bef
Jason Ish <ish@unx.ca>
2016-07-13 16:39:39 -0600

    add smtp lua output test

---
+++ output-lua-smtp/smtp.lua
@@ -0,0 +1,34 @@
+-- simple fast-log to file lua module
+name = "smtp_lua.log"
+
+function init (args)
+    local needs = {}
+    needs["protocol"] = "smtp"
+    return needs
+end
+
+function setup (args)
+    filename = SCLogPath() .. "/" .. name
+    file = assert(io.open(filename, "a"))
+    SCLogInfo("Log Filename " .. filename)
+    count = 0
+end
+
+function log(args)
+   ts = SCPacketTimeString()
+   from = SMTPGetMailFrom()
+   to = SMTPGetRcptList()
+   to_string = ""
+   for key,val in pairs(to) do
+      to_string = to_string .. val
+   end
+   file:write(ts .. " FROM " .. from .. " TO {" .. to_string .. "}\n")
+   file:flush()
+
+   count = count + 1
+end
+
+function deinit (args)
+    SCLogInfo ("transactions logged: " .. count);
+    file:close(file)
+end
