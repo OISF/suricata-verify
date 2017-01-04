78a0e67f0638ecebf2c6b05ff52dcf1826c5a87c
Jason Ish <ish@unx.ca>
2017-01-04 11:59:38 -0600

    proto-mismatch-http-ssh: test app-layer mismatch
    Tests that an app-layer mismatch is detected.

---
+++ proto-mismatch-http-ssh/check.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+failed=no
+
+# We should get a "SURICATA Applayer Mismatch protocol both
+# directions" alert.
+n=$(cat output/eve.json | jq -c 'select(.alert.signature_id == 2260000)' | wc -l)
+if [ "$n" != 1 ]; then
+    echo "expected 1 event with SID 2260000"
+    failed=yes
+fi
+
+# We should have a flow event with app_proto = http and app_proto_tc = ssh.
+n=$(cat output/eve.json | jq -c 'select(.event_type == "flow") | select(.app_proto == "http") | select(.app_proto_tc == "ssh")' | wc -l)
+if [ "$n" != 1 ]; then
+    echo "expected 1 event with app_proto http and app_proto_tc ssh"
+    failed=yes
+fi
+
+if [ "${failed}" = "yes" ]; then
+    exit 1
+fi
+
+exit 0
+
