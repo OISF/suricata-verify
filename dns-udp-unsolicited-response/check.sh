973ccf648499865d0f9576910a15a879df74822d
Jason Ish <ish@unx.ca>
2017-01-10 10:48:49 -0600

    new test: dns-udp-unsolicited-response
    Test for unsolicited DNS response on existing flow.

---
+++ dns-udp-unsolicited-response/check.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# Check for 1 DNS request.
+n=$(cat output/eve.json | jq -c 'select(.dns.type == "query")' | wc -l)
+if [ ${n} -ne 1 ]; then
+    exit 1
+fi
+
+# Check for 1 DNS response.
+n=$(cat output/eve.json | jq -c 'select(.dns.type == "answer")' | wc -l)
+if [ ${n} -ne 2 ]; then
+    exit 1
+fi
+
+# Check for one alert.
+n=$(cat output/eve.json | jq -c 'select(.event_type == "alert")' | wc -l)
+if [ ${n} -ne 1 ]; then
+    exit 1
+fi
+
+exit 0
