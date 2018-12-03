be8f8dead6bfef72604a839be689980055dcded2
Jason Ish <ish@unx.ca>
2018-12-03 10:38:01 -0600

    dns tests: enable eve v1 tests for Rust builds.
    Suricata branch ish/rust-dns-v1/v1 brings DNS v1 logging
    support for Rust.

--- tests/dns-udp-nxdomain-soa/check.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-. ${TOPDIR}/util/functions.sh
-
-# Look for 2 responses with rcode == "NXDOMAIN".
-n=$(jq_count eve.json 'select(.dns.rcode == "NXDOMAIN")')
-assert_eq 2 "$n" "nxdomain responses"
-
-exit 0
