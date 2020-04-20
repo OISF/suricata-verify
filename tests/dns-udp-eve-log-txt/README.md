56bc760b37319d45ed0573c94c54af6c50d2ba16
Jason Ish <jason.ish@oisf.net>
2020-04-20 11:39:54 -0600

    dns txt test: break into v1 and v2 tests
    Eliminating the check.sh test at the same time.

--- tests/dns-udp-eve-log-txt/README.md
+++
@@ -1,2 +0,0 @@
-Test that a TXT record is extracted and logged correctly to Eve.
-
--- tests/dns-udp-eve-log-txt/check.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-set -e
-
-txt=$(cat eve.json | \
-	  jq -c 'select(.dns.type == "answer") | select(.dns.rrtype == "TXT") | .dns.rdata')
-test "${txt}" = '"v=spf1 include:_spf.google.com ~all"'
-
-
