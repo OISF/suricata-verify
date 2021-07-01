2b2f5dd24dcbd90269413ab2bcdbcca14e14618a
Juliana Fajardini <jufajardini@gmail.com>
2021-07-01 19:57:11 +0100

    tests: add lua SCFlowTuple test

---
+++ tests/lua-scflowtuple/scflowtuple.lua
@@ -0,0 +1,30 @@
+-- simple SCFlowTuple log test
+name = "scflow-tuple.log"
+
+function init(args)
+    local needs = {}
+    needs["type"] = "flow"
+    return needs
+end
+
+function setup(args)
+    filename = SCLogPath() .. "/" .. name
+    file = assert(io.open(filename, "a"))
+    SCLogInfo("Lua SCFlowTuple Log Filename " .. filename)
+    flow = 0
+end
+
+function log(args)
+    startts = SCFlowTimeString()
+    ipver, srcip, dstip, proto, sp, dp = SCFlowTuple()
+    proto_string = SCFlowAppLayerProto()
+
+    file:write ("{" .. startts .. " [**]\nSCFlowTuple is\nIP Version:  " .. ipver .. "\nSrc: " .. srcip .. ":" .. sp .. " -> Dst: " .. dstip .. ":" .. dp .. " [**] Protocol: " .. proto_string .. "(" .. proto .. ")}\n")
+    file:flush()
+    flow = flow + 1
+end
+
+function deinit(args)
+    SCLogInfo ("Flow tuples logged: " .. flow);
+    file:close(file)
+end
