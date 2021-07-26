c56bde77d9b0e85637c82bda1205456aac02cd2d
Juliana Fajardini <jufajardini@gmail.com>
2021-07-26 14:49:23 +0100

    tests: add test for SCFlowStats function

---
+++ tests/lua-scflowstats/lua-scflowstats.lua
@@ -0,0 +1,26 @@
+-- lua_pushinteger output test for SCFlowStats and ...
+name = "lua-scflowstats.log"
+
+function init(args)
+    local needs = {}
+    needs["type"] = "flow"
+    return needs
+end
+
+function setup(args)
+    filename = SCLogPath() .. "/" .. name
+    file = assert(io.open(filename, "a"))
+    SCLogInfo("lua SCFlowStats Log Filename " .. filename)
+end
+
+function log(args)
+    timestring = SCFlowTimeString()
+    tscnt, tsbytes, tccnt, tcbytes = SCFlowStats()
+
+   file:write ("[**] " .. timestring .. "\nSCFlowStats is\nPacket count to server:  " .. tscnt .. "\nByte count to server: " .. tsbytes .. "\nPacket count to client: " .. tccnt .. "\nByte count to client: " .. tcbytes .. "\n[**]")
+    file:flush()
+end
+
+function deinit(args)
+    file:close(file)
+end
