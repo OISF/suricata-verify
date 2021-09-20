6fcc4bd22dc59931cda5b272bb99d1f8d221b3ad
Jason Ish <jason.ish@oisf.net>
2021-09-20 12:27:24 -0600

    detect-filestore-config-*: convert from check.sh to test.yaml

--- tests/detect-filestore-config-01/check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if  grep -q "Warning: Rule requires file-store but the output file-store is not enabled." $OUTPUT_DIR/rules_analysis.txt; then
-    echo "Pattern found in rules_analysis.txt"
-    exit 1
-fi
--- tests/detect-filestore-config-02/check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if ! grep -q "Warning: Rule requires file-store but the output file-store is not enabled." $OUTPUT_DIR/rules_analysis.txt; then
-    echo "Pattern not found"
-    exit 1
-fi
-exit 0
