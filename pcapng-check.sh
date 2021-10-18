5b3a139c7c044e84ef24f669b8f2029262dc1923
Victor Julien <victor@inliniac.net>
2021-10-18 15:16:57 +0200

    ci: fix pcapng check on ubuntu

--- pcapng-check.sh
+++ pcapng-check.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
   
-set -x
+#set -x
 
-PCAPNG=$(find tests/ -type f|xargs -L1 file|grep 'pcap-ng capture file')
-PCAPNGCNT=$(echo -n $PCAPNG | wc -l)
+PCAPNGCNT=$(find tests/ -type f|xargs -L1 file|grep -E "(pcap\-ng|pcapng) capture file"|wc -l)
 if [ $PCAPNGCNT -ne 0 ]; then
-    echo "pcap-ng tests found"
-    echo $PCAPNG
+    echo "$PCAPNGCNT pcap-ng files found:"
+    echo
+    find tests/ -type f|xargs -L1 file|grep -E "(pcap\-ng|pcapng) capture file"
+    echo
     echo "PCAP-NG files are currently not allowed for tests due to not "
     echo "all platforms supporting it at this time. Please convert the "
     echo "pcap file(s) to regular pcap format: "
