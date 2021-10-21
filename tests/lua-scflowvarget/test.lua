119ebf50479e9557a99c92157bd2e077e8d127a5
Philippe Antoine <contact@catenacyber.fr>
2021-10-21 13:43:01 +0200

    lua: adds test about ScFlowVarGet
    cf redmine 2094

---
+++ tests/lua-scflowvarget/test.lua
@@ -0,0 +1,18 @@
+function init (args)
+    local needs = {}
+    needs["http.request_headers"] = tostring(true)
+    needs["flowvar"] = {"TestVar"}
+    return needs
+end
+
+function match(args)
+    print "Before loading Variable"
+    testVar = ScFlowvarGet(0);
+    if testVar then
+      print "testVar is set"
+    else
+      print "testVar is not set"
+      return 0
+    end
+    return 1
+end
