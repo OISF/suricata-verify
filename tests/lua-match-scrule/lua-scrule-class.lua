dcfbb134f2af90019e7e2acebcf1fa51583fa5e7
Benjamin Wilkins <benjamin.wilkins@uwaterloo.ca>
2021-11-02 15:24:21 -0400

    lua: Test SCRule* functions for match scripts

---
+++ tests/lua-match-scrule/lua-scrule-class.lua
@@ -0,0 +1,14 @@
+function init(args)
+    local needs = {}
+    return needs
+end
+
+function match(args)
+    msg, prio = SCRuleClass()
+
+    if msg == "Potentially Bad Traffic" and prio == 2 then
+        return 1
+    else
+        return 0
+    end
+end
