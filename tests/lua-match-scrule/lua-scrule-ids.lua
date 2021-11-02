bf6cca5adf136f4400681f1ba35a9ffc0721b1f1
Benjamin Wilkins <benjamin.wilkins@uwaterloo.ca>
2021-11-02 15:24:21 -0400

    lua: Test SCRule* functions for match scripts

---
+++ tests/lua-match-scrule/lua-scrule-ids.lua
@@ -0,0 +1,14 @@
+function init(args)
+    local needs = {}
+    return needs
+end
+
+function match(args)
+    sid, rev, gid = SCRuleIds()
+
+    if sid == 1 and rev == 7 and gid == 1 then
+        return 1
+    else
+        return 0
+    end
+end
