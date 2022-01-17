435141bee773e6dab26e8fe7f8e996cdfea48f2b
Modupe Falodun <falodunmodupeola@gmail.com>
2022-01-17 22:09:10 +0100

    Uricontent: add tests for uricontent
    Task: 4911

---
+++ tests/uricontent/uricontent-03/writepcap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from scapy.all import *
+
+pkts = []
+
+pkts += Ether(dst='ff:ff:ff:ff:ff:ff', src='00:01:02:03:04:05')/ \
+    Dot1Q(vlan=6)/ \
+    IP(dst='192.168.1.1', src='192.168.1.5')/TCP(sport=41424, dport=80, flags='P''A')/"POST /one/two/three/six HTTP/1.0\r\nUser-Agent: Mozilla/1.0\r\nCookie: hellocatch\r\n\r\n"
+
+wrpcap('input.pcap', pkts)
