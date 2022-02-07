75c8cc4de50fdbb98c6a00ff77c4e608f0e7be47
Modupe Falodun <falodunmodupeola@gmail.com>
2022-02-07 17:10:16 +0100

    detect-pcre: add tests
    Task: 4911

---
+++ tests/detect-pcre/detect-pcre-02/writepcap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from scapy.all import *
+
+pkts = []
+
+pkts += Ether(dst='ff:ff:ff:ff:ff:ff', src='00:01:02:03:04:05')/ \
+    Dot1Q(vlan=6)/ \
+    IP(dst='1.2.3.4', src='5.6.7.8')/TCP(sport=6666, dport=80, flags='P''A')/"GET / HTTP/1.1\r\nHost: www.emergingthreats.net\r\nUser-Agent: Mozilla/1.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9;q=0.8\r\nAccept-Encoding: gzip,deflate\r\nTransfer-Encoding: chunked\r\nContent-Type: text/html; charset=utf-8\r\n\r\n15\r\n<!DOC"
+pkts += Ether(dst='ff:ff:ff:ff:ff:ff', src='00:01:02:03:04:05')/ \
+    Dot1Q(vlan=6)/ \
+    IP(dst='1.2.3.4', src='5.6.7.8')/TCP(sport=6666, dport=80, flags='P''A')/"<!DOCTYPE html PUBLIC\r\n0\r\n\r\n"
+    
+wrpcap('input.pcap', pkts)
---
+++ tests/detect-pcre/detect-pcre-04/writepcap.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from scapy.all import *
+
+pkts = []
+
+pkts += Ether(dst='ff:ff:ff:ff:ff:ff', src='00:01:02:03:04:05')/ \
+    Dot1Q(vlan=6)/ \
+    IP(dst='1.2.3.4', src='5.6.7.8')/TCP(sport=6666, dport=80, flags='P''A')/"POST / HTTP/1.0\r\nUser-Agent: Mozilla/1.0\r\nCookie: dummy 1234\r\n\r\n"
+wrpcap('input.pcap', pkts)
