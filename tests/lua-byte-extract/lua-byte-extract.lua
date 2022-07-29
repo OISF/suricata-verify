af959459a26b9fdbb84ffa5921ee5a60b07ef78b
Jeff Lucovsky <jlucovsky@oisf.net>
2022-07-29 08:53:40 -0400

    tests/lua: Byte-extract/byte-math tests
    Issue: 2871

--- tests/lua-byte-extract/lua-byte-extract.lua
+++ tests/lua-byte-extract/lua-byte-extract.lua
@@ -8,9 +8,10 @@ function match(args)
     local var1 = SCByteVarGet(0)
     local var2 = SCByteVarGet(1)
 
-    if string.pack(">i4", var1) == "HTTP" and string.pack(">i4", var2) == "/1.1" then
-        return 1
-    else 
-        return 0
+    if var1 and var2 then
+        if var1 == 0x48545450 and var2 == 0x2f312e31 then
+            return 1
+        end
     end
+    return 0
 end
