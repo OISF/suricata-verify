407c04719cb8601c5ea9605df8727e0d68d82f88
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-03 10:53:41 +0100

    add test for detect lua 01

---
+++ tests/detect-lua-01/test1.lua
@@ -0,0 +1,27 @@
+function init (args)
+    local needs = {}
+    needs["http.request_headers"] = tostring(true)
+    needs["flowvar"] = {"cnt"}
+    return needs
+    end
+
+function match(args)
+    a = ScFlowvarGet(0)
+    if a then
+        a = tostring(tonumber(a)+1)
+        print ((a))
+        ScFlowvarSet(0, a, #a)
+    else
+        a = tostring(1)
+        print ((a))
+        ScFlowvarSet(0, a, #a)
+    end
+
+    print ("pre check: " .. (a))
+    if tonumber(a) == 2 then
+        print "match"
+        return 1
+    end
+    return 0
+end
+return 0
