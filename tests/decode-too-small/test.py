c20e1f1dfecf30f8d84cc77adbaf7949816c5110
Cole Dishington <Cole.Dishington@alliedtelesis.co.nz>
2023-05-25 11:25:08 +1200

    decode-event: Add test for too small UDP and TCP packets
    Add test for IPv4 and IPv6 packets that set proto/next-header to UDP and
    TCP but have no payload.
    
    Bug: #6086.

---
+++ tests/decode-too-small/test.py
@@ -0,0 +1,13 @@
+from scapy.all import Ether, IP, IPv6, PcapWriter
+
+with PcapWriter('test.pcap') as pcap:
+    # TCP and UDP too small packets
+    s_mac = 'cb:cf:2b:50:a7:61'
+    d_mac = '49:a2:25:1a:07:4a'
+    proto_tcp = 6
+    proto_udp = 17
+
+    pcap.write(Ether(src=s_mac, dst=d_mac) / IP(src='1.1.1.1', dst='2.2.2.2', proto=proto_tcp))
+    pcap.write(Ether(src=s_mac, dst=d_mac) / IP(src='1.1.1.1', dst='2.2.2.2', proto=proto_udp))
+    pcap.write(Ether(src=s_mac, dst=d_mac) / IPv6(src='fd01::1.1.1.1', dst='fd02::2.2.2.2', nh=proto_tcp))
+    pcap.write(Ether(src=s_mac, dst=d_mac) / IPv6(src='fd01::1.1.1.1', dst='fd02::2.2.2.2', nh=proto_udp))
