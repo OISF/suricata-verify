600234965963b19340a39ab34e0a00c7797a3aaa
Arne Welzel <arne.welzel@corelight.com>
2024-02-20 12:07:34 +0100

    test-dump-counters: Test dump-counters via unix-command socket
    Ticket: 6732

---
+++ tests/test-dump-counters/test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# This test runs Suricata as background process listening on the lo
+# interface with a BPF filter that makes it unlikely that we'll be
+# capturing anything. Rule loading is disabled, too.
+set -u
+
+export PYTHONPATH=${SRCDIR}/python:${PYTHONPATH:-}
+
+POLL=0.1
+TIMEOUT=10
+SURICATASC=${SRCDIR}/python/bin/suricatasc
+BPF_FILTER='src host 8.8.8.8 and dst host 100.64.0.1'
+UNIX_COMMAND_FILENAME=${OUTPUT_DIR}/suricata.sock
+
+function run_suricatasc {
+    "${SURICATASC}" -c "$@" "${UNIX_COMMAND_FILENAME}"
+}
+
+if ! run_suricatasc version -h > /dev/null; then
+    echo "suricatasc not functional" >&2
+    exit 1
+fi
+
+timeout -k 1 $TIMEOUT "${SRCDIR}/src/suricata" -v \
+    -c "${SRCDIR}/suricata.yaml" \
+    -l "${OUTPUT_DIR}" \
+    --pcap=lo \
+    --runmode=workers \
+    --set capture.disable-offloading=false \
+    --set capture.checksum-validation=none \
+    --set pcap.1.threads=2 \
+    --set flow.managers=3 \
+    --set flow.recyclers=5 \
+    --set stats.interval=1 \
+    --set rule-files.0=/dev/null \
+    --set unix-command.filename="${UNIX_COMMAND_FILENAME}" \
+    "${BPF_FILTER}" &
+
+SURICATA_PID=$!
+
+# Cleanup
+trap '{ echo trap; kill ${SURICATA_PID} ; exit 1; }' SIGINT SIGTERM ERR
+
+function suricata_is_alive {
+    ps --pid ${SURICATA_PID} > /dev/null
+}
+
+# suricatasc exits with 1 until stats are synchronized
+while ! run_suricatasc dump-counters && suricata_is_alive; do
+    sleep $POLL
+done
+
+run_suricatasc dump-counters > "${OUTPUT_DIR}/dump-counters.json"
+
+run_suricatasc shutdown
+
+trap '' SIGINT SIGTERM ERR
+
+wait $SURICATA_PID
