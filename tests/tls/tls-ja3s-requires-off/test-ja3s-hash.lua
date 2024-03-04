83ae108bd6f5b78893d3c6afd77e3a8d55d46ccb
Sascha Steinbiss <satta@debian.org>
2024-03-04 19:22:16 +0100

    ja3: adjust and add tests

---
+++ tests/tls/tls-ja3s-requires-off/test-ja3s-hash.lua
@@ -0,0 +1,18 @@
+function init(args)
+    local needs = {}
+    needs["tls"] = tostring(true)
+    return needs
+end
+
+function match(args)
+    hash = Ja3SGetHash()
+    if hash == nil then
+        return 0
+    end
+
+    if hash == "5d79edf64e03689ff559a54e9d9487bc" then
+        return 1
+    end
+
+    return 0
+end
