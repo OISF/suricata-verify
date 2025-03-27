1acc44e71ce872d9565ddf790d21aa5e7e9cb76f
Jason Ish <jason.ish@oisf.net>
2025-03-27 11:20:05 -0600

    tests: add dnp3 lua state
    This is very minimal, should be expanded on.
    
    Ticket: #7601

---
+++ tests/dnp3/dnp3-lua/rule.lua
@@ -0,0 +1,31 @@
+local dnp3 = require("suricata.dnp3")
+
+function init (args)
+   return {dnp3 = true}
+end
+
+function match(args)
+   -- No args for DNP3.
+   
+   -- Get transaction.
+   local tx = dnp3.get_tx()
+
+   if not tx["is_request"] then
+      return 0
+   end
+
+   local request = tx["request"]
+   if request == nil then
+      return 0
+   end
+
+   if not request["done"] then
+      return 0
+   end
+
+   if not request["complete"] then
+      return 0
+   end
+
+   return 1
+end
