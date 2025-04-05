09c2724820ba1b55065037829561f3655f24fd1d
Philippe Antoine <pantoine@oisf.net>
2025-04-05 22:54:12 +0200

    ssh: adds test for lua
    Ticket: 7607

---
+++ tests/ssh-lua-rules/test-ssh.lua
@@ -0,0 +1,18 @@
+local ssh = require("suricata.ssh")
+
+function init (args)
+   local needs = {}
+   return needs
+end
+
+function match(args)
+   local tx = ssh.get_tx()
+   local proto = tx:server_proto()
+   if proto == "2.0" then
+      local soft = tx:server_software()
+      if soft == "OpenSSH_7.4" then
+         return 1
+      end
+   end
+   return 0
+end
