b9e2bc1f29728bd4cb77b6edeefe5a0d791f2b87
Jason Ish <jason.ish@oisf.net>
2026-02-27 16:05:49 -0600

    misc: filter check requires cleanups
    - Move require expressions in filter check to requires object
    - Error if an unknown key is provided in a filter check

--- run.py
+++ run.py
@@ -533,6 +533,9 @@ class StatsCheck:
 class FilterCheck:
 
     def __init__(self, config, outdir, suricata_config, test_version, script_cwd=None):
+        for key in config:
+            if key not in ["count", "match", "filename", "requires"]:
+                raise Exception("Unexpected key in filter check: {}".format(key))
         self.config = config
         self.outdir = outdir
         self.suricata_config = suricata_config
@@ -542,19 +545,7 @@ class FilterCheck:
 
     def run(self):
         requires = self.config.get("requires", {})
-        req_version = self.config.get("version")
-        min_version = self.config.get("min-version")
-        lt_version = self.config.get("lt-version")
-        if req_version is not None:
-            requires["version"] = req_version
-        if min_version is not None:
-            requires["min-version"] = min_version
-        if lt_version is not None:
-            requires["lt-version"] = lt_version
         check_filter_test_version_compat(requires, self.test_version)
-        feature = self.config.get("feature")
-        if feature is not None:
-            requires["features"] = [feature]
         check_requires(requires, self.suricata_config, self.script_cwd)
 
         if "filename" in self.config:
